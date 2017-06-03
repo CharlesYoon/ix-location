50c0ca702411431001568f2ec6bad7325c854516
Miki von Ketelhodt <vonketelhodtm@gmail.com>
2017-06-03 14:14:23 +0200

    Added apple maps with region

--- ixLocation/AppleMapsViewController.swift
+++ ixLocation/AppleMapsViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import MapKit
 
 class AppleMapsViewController: UIViewController {
 
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // You can set the map to be different types
+        //map.mapType = MKMapType.standard
+        
+        // A region represents the area of the map that we want to view
+        // It consists of a location (CLLocationCoordinate2D) and a Span (MKCoordinateSpanMak)
+        let location = CLLocationCoordinate2D(latitude: -33.918861,longitude: 18.423300)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        map.setRegion(region, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
--- ixLocation/GoogleMapsViewController.swift
+++ ixLocation/GoogleMapsViewController.swift
@@ -13,8 +13,9 @@ class GoogleMapsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // A camera represents how we look at the map, where we are looking and how far in/out (zoom) we are looking at the map at
-        let camera = GMSCameraPosition.camera(withLatitude: -33.918861, longitude: 18.423300, zoom: 8.0)
+        let camera = GMSCameraPosition.camera(withLatitude: -33.918861, longitude: 18.423300, zoom: 12.0)
         
         // The MapView inherits from UIView and can be assigned to a UIView object. 
         // In our case, the entire view the controller manages
