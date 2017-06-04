3eca409ec4cb37f8feb81b23b4f9d4b5436e10d2
Miki von Ketelhodt <vonketelhodtm@gmail.com>
2017-06-04 14:12:15 +0200

    Added location permission request description and requesting user location

--- ixLocation/AppleMapsViewController.swift
+++ ixLocation/AppleMapsViewController.swift
@@ -9,15 +9,15 @@
 import UIKit
 import MapKit
 
-class AppleMapsViewController: UIViewController {
+class AppleMapsViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var map: MKMapView!
     
+    var locationManager: CLLocationManager!
+    var currentUserLocation: CLLocation!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // You can set the map to be different types
-        //map.mapType = MKMapType.standard
         
         // A region represents the area of the map that we want to view
         // It consists of a location (CLLocationCoordinate2D) and a Span (MKCoordinateSpanMak)
@@ -26,6 +26,17 @@ class AppleMapsViewController: UIViewController {
         let region = MKCoordinateRegion(center: location, span: span)
         
         map.setRegion(region, animated: true)
+        
+        // 
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        //locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +44,26 @@ class AppleMapsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // Get the users location from the array of locations
+        let userLocation: CLLocation = locations[0] as CLLocation
+        
+        // You can call stopUpdatingLocation() to stop listening for location updates
+        // manager.stopUpdatingLocation()
+        
+        print("user latitude = \(userLocation.coordinate.latitude)")
+        print("user longitude = \(userLocation.coordinate.longitude)")
+        
+        // Store reference to the users location in the class instance (self)
+        self.currentUserLocation = userLocation
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+        // An error occurred trying to retrieve users location
+        print("Error \(error)")
+    }
 
 }
 
