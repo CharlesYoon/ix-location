a70e7ba284c1c4cb593f223b26c6ab6e7607259e
Miki von Ketelhodt <vonketelhodtm@gmail.com>
2017-06-19 16:04:15 +0200

    FeatureB on ActivityDto

--- ixLocation/ActivityDto.swift
+++ ixLocation/ActivityDto.swift
@@ -17,6 +17,7 @@ class ActivityDto: Decodable, Glossy {
     var image: UIImage?
     var location: GeoPoint?
     var imageUrl: String?
+    var featureB: String?
     
     init?() {
         self.name = ""
